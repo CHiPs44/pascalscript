32c8d3a2a62f899fa814a0e5522879e8f1220d83
CHiPs <CHiPs44@users.noreply.github.com>
2023-08-28 14:31:40 +0000

    Strings: empty + double quotes inside

--- examples/00-hello.pas
+++ examples/00-hello.pas
@@ -1,17 +1,20 @@
 program hello;
 { Won't work with first versions }
 const
+  EMPTYSTRING = '';
+  QUOTESTRING = '''';
+  HelloWorld4 = 'Hello, ''world!';
   ZZZ = 'Z';
   HelloWorld0 = 'Hello, world!';
   HelloWorld1 = 12345678;
-  // HelloWorld2 = 123456789023457890123456789023457890;
-  // HelloWorld3 = 4294967295;
-  // HelloWorld3 = 2147483648;
+  HelloWorld2 = 123456789023457890123456789023457890;
+  HelloWorld3 = 4294967295;
+  HelloWorld3 = 2147483648;
 // var
 //   test: integer;
 begin
   // test := 1 + 2;
   Write(HelloWorld0);
   WriteLn(HelloWorld1);
-  WriteLn('Hello, world!');
+  // WriteLn('Hello, world!');
 end.
