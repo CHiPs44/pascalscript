8480418cb7b97fa5ce324337e958518e409f4df4
CHiPs <CHiPs44@users.noreply.github.com>
2023-08-29 15:43:23 +0000

    char/string + case insensitive keywords

--- examples/00-hello.pas
+++ examples/00-hello.pas
@@ -1,21 +1,23 @@
-program hello;
+Program hello;
 { Won't work with first versions }
-const
+Const
   EMPTYSTRING = '';
   QUOTESTRING = '''';
   HelloWorld4 = 'Hello, ''world''!';
   ZZZ = 'Z';
   HelloWorld0 = 'Hello, world!';
-  LOREM_IPSUM='"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."';
-  // HelloWorld1 = 12345678;
-  // HelloWorld2 = 123456789023457890123456789023457890;
-  // HelloWorld3 = 4294967295;
-  // HelloWorld3 = 2147483648;
-// var
-//   test: integer;
-begin
-  // test := 1 + 2;
+  LOREM_IPSUM_1 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+  LOREM_IPSUM_2 = 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.';
+  LOREM_IPSUM_3 = 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+  {HelloWorld1 = 12345678;
+  HelloWorld2 = 123456789023457890123456789023457890;
+  HelloWorld3 = 4294967295;
+  HelloWorld3 = 2147483648;}
+var
+  test: integer;
+Begin
+  test := 1 + 2;
   Write(HelloWorld0);
   WriteLn(HelloWorld1);
   // WriteLn('Hello, world!');
-end.
+End.
