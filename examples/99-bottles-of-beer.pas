767a867f60b2a18e058d8d40f97beb1931999500
CHiPs <CHiPs44@users.noreply.github.com>
2024-06-26 12:17:00 +0000

    Added 99 Bottles Of Bear example from Roseta Code

---
+++ examples/99-bottles-of-beer.pas
@@ -0,0 +1,28 @@
+{ from https://rosettacode.org/wiki/99_Bottles_of_Beer/Pascal }
+
+Program BottlesOfBeer;
+
+Var 
+  i: Integer;
+
+Begin
+  For i := 99 Downto 1 Do
+    If i <> 1 Then
+      Begin
+        WriteLn(i, ' bottles of beer on the wall');
+        WriteLn(i, ' bottles of beer');
+        WriteLn('Take one down, pass it around');
+        If i = 2 Then
+          WriteLn('One bottle of beer on the wall')
+        Else
+          WriteLn(i - 1, ' bottles of beer on the wall');
+        WriteLn;
+      End
+    Else
+      Begin
+        WriteLn('One bottle of beer on the wall');
+        WriteLn('One bottle of beer');
+        WriteLn('Take one down, pass it around');
+        WriteLn('No more bottles of beer on the wall');
+      End
+End.
