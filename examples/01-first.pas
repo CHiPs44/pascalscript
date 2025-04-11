d54bedca9b7c1f7d724676703cfcae240f9644a5
CHiPs44 <chips@chips44.fr>
2025-04-11 08:38:38 +0200

    EBNF proof read + example 01

--- examples/01-first.pas
+++ examples/01-first.pas
@@ -5,12 +5,15 @@ Var
     a: Integer;
     b: Integer;
     c: Integer;
+    d: Integer;
 Begin
     a := +1;
     b := -2;
     {c := a - b + THOUSAND;}
     c := (a - b) * (THOUSAND + THOUSAND) Div THOUSAND;
+    d := 6;
     WriteLn('a', '=', a);
     WriteLn('b', '=', b);
     WriteLn('c', '=', c);
+    WriteLn('?', '=', c = d);
 End.
