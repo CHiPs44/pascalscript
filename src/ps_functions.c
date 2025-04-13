bb105bac471be3a10eb3f5957a481c32c28acee3
CHiPs44 <chips@chips44.fr>
2025-04-13 22:11:17 +0200

    fix copyleft year

--- src/ps_functions.c
+++ src/ps_functions.c
@@ -1,6 +1,6 @@
 /*
     This file is part of the PascalScript Pascal interpreter.
-    SPDX-FileCopyrightText: 2024 Christophe "CHiPs" Petit <chips44@gmail.com>
+    SPDX-FileCopyrightText: 2025 Christophe "CHiPs" Petit <chips44@gmail.com>
     SPDX-License-Identifier: GPL-3.0-or-later
 */
 
