6afe007d52dcb8fd07a5eaedacc973dd63f80f15
CHiPs44 <chips@chips44.fr>
2025-04-29 14:03:33 +0200

    example rework

--- examples/00-hello.pas
+++ examples/00-hello.pas
@@ -1,12 +1,13 @@
 (* Won't work with first versions *)
 Program HelloWorld;
 Const
-  QUOTE_CHAR = '''';
-  Z_CHAR = 'Z';
-  EMPTY_STRING = '';
-  HELLO_WORLD_0 = 'Hello, world!';
-  HELLO_WORLD_1 = 'Hello, ''world''!';
-  HELLO_WORLD_MAX = {
+  QUOTE_CHAR      = '''';
+  Z_CHAR          = 'Z';
+  EMPTY_STRING    = '';
+  HELLO_WORLD_0   = '''Hello, world!''';
+  HELLO_WORLD_1   = 'Hello, ''world''!';
+  HELLO_WORLD_MAX = 
+{
                                                                                                    1         1         1         1         1         1         1         1         1         1         2         2         2         2         2         2
          1         2         3         4         5         6         7         8         9         0         1         2         3         4         5         6         7         8         9         0         1         2         3         4         5
 12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234
@@ -15,10 +16,10 @@ Const
 ;
 Begin
   WriteLn('Hello, world!');
-  WriteLn(QUOTE_CHAR);
-  WriteLn(Z_CHAR);
-  WriteLn(EMPTY_STRING);
-  WriteLn(HELLO_WORLD_0);
-  WriteLn(HELLO_WORLD_1);
-  WriteLn(HELLO_WORLD_MAX);
+  WriteLn('Quote: ', QUOTE_CHAR);
+  WriteLn('Z    : ', Z_CHAR);
+  WriteLn('Empty: ', EMPTY_STRING);
+  WriteLn('Zero : ', HELLO_WORLD_0);
+  WriteLn('One  : ', HELLO_WORLD_1);
+  WriteLn('Max  : ', HELLO_WORLD_MAX);
 End.
