ae046725af52bf4e51c51cdd47cf5db2585286e5
CHiPs <CHiPs44@users.noreply.github.com>
2025-09-25 13:49:26 +0000

    font + config sizes

--- src/ps_config.c
+++ src/ps_config.c
@@ -7,7 +7,9 @@
 #include <stdio.h>
 
 #include "ps_config.h"
+#include "ps_symbol.h"
 #include "ps_system_types.h"
+#include "ps_value.h"
 #include "ps_version.h"
 
 #define STRING(Z) #Z
@@ -16,9 +18,9 @@
 
 #define REPORT_STRING(__MACRO__) printf("┃ %-36s ┃ %-37s ┃\n", #__MACRO__, STRING(__MACRO__))
 
-#define REPORT_SECTION(__TITLE__)                                                                                      \
-    printf("┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫\n");                      \
-    printf("┃ %-76s ┃\n", __TITLE__);                                                                                  \
+#define REPORT_SECTION(__TITLE__)                                                                 \
+    printf("┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫\n"); \
+    printf("┃ %-76s ┃\n", __TITLE__);                                                             \
     printf("┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫\n")
 
 void ps_config_report()
@@ -39,41 +41,43 @@ void ps_config_report()
     REPORT_FORMAT(PS_BITNESS, "d");
     REPORT_SECTION("*** INTEGER TYPE ***");
     REPORT_STRING(PS_INTEGER);
+    REPORT_FORMAT(PS_INTEGER_SIZE, "u");
     REPORT_FORMAT(PS_INTEGER_MIN, PS_INTEGER_FMT_10);
     REPORT_FORMAT(PS_INTEGER_MAX, PS_INTEGER_FMT_10);
     REPORT_FORMAT(PS_INTEGER_FMT_10, "s");
     REPORT_FORMAT(PS_INTEGER_FMT_16, "s");
     REPORT_SECTION("*** UNSIGNED TYPE ***");
     REPORT_STRING(PS_UNSIGNED);
+    REPORT_FORMAT(PS_UNSIGNED_SIZE, "u");
     REPORT_FORMAT(PS_UNSIGNED_MAX, PS_UNSIGNED_FMT_10);
     REPORT_FORMAT(PS_UNSIGNED_FMT_10, "s");
     REPORT_FORMAT(PS_UNSIGNED_FMT_16, "s");
     REPORT_SECTION("*** BOOLEAN TYPE ***");
     REPORT_STRING(PS_BOOLEAN);
+    REPORT_FORMAT(PS_BOOLEAN_SIZE, "u");
     REPORT_SECTION("*** REAL TYPE ***");
     REPORT_STRING(PS_REAL);
+    REPORT_FORMAT(PS_REAL_SIZE, "u");
     REPORT_FORMAT(PS_REAL_MIN, "G" /*PS_REAL_FMT*/);
     REPORT_FORMAT(PS_REAL_MAX, "G" /*PS_REAL_FMT*/);
     REPORT_FORMAT(PS_REAL_EPSILON, "G" /*PS_REAL_FMT*/);
     REPORT_FORMAT(PS_REAL_FMT, "s");
     REPORT_SECTION("*** CHARACTER TYPE ***");
     REPORT_STRING(PS_CHAR);
+    REPORT_FORMAT(PS_CHAR_SIZE, "u");
     REPORT_FORMAT(PS_CHAR_MAX, "u");
     REPORT_SECTION("*** STRING TYPE ***");
     REPORT_STRING(PS_STRING_LEN_TYPE);
     REPORT_FORMAT(PS_STRING_MAX_LEN, "u");
-    REPORT_STRING(PS_STRING_REF_TYPE);
+    REPORT_FORMAT(PS_STRING_SIZE, "u");
+    // REPORT_STRING(PS_STRING_REF_TYPE);
     REPORT_SECTION("*** IDENTIFIER TYPE ***");
     REPORT_FORMAT(PS_IDENTIFIER_LEN, "u");
     REPORT_FORMAT(PS_IDENTIFIER_SIZE, "u");
     REPORT_SECTION("*** SIZES ***");
-    REPORT_FORMAT(PS_INTEGER_SIZE, "u");
-    REPORT_FORMAT(PS_UNSIGNED_SIZE, "u");
-    REPORT_FORMAT(PS_REAL_SIZE, "u");
-    REPORT_FORMAT(PS_BOOLEAN_SIZE, "u");
-    REPORT_FORMAT(PS_CHAR_SIZE, "u");
-    REPORT_FORMAT(PS_STRING_SIZE, "u");
-    REPORT_FORMAT(PS_FILE_SIZE, "u");
-    REPORT_FORMAT(PS_POINTER_SIZE, "u");
+    // REPORT_FORMAT(PS_FILE_SIZE, "u");
+    // REPORT_FORMAT(PS_POINTER_SIZE, "u");
+    REPORT_FORMAT(PS_VALUE_SIZE, "u");
+    REPORT_FORMAT(PS_SYMBOL_SIZE, "u");
     printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");
 }
