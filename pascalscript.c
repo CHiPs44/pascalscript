d15dde80153872dcddddef3a4ea84eca82d4c544
CHiPs44 <chips@chips44.fr>
2026-08-03 22:49:53 +0200

    debug 080 math WIP (unary operator has no result type)

--- pascalscript.c
+++ pascalscript.c
@@ -35,7 +35,8 @@
 // #define DEBUG_SOURCE "examples/010-operators.pas"
 // #define DEBUG_SOURCE "examples/024-for-do.pas"
 // #define DEBUG_SOURCE "examples/021-repeat-until.pas"
-#define DEBUG_SOURCE "examples/070-random.pas"
+// #define DEBUG_SOURCE "examples/070-random.pas"
+#define DEBUG_SOURCE "examples/080-math.pas"
 // #define DEBUG_SOURCE "examples/130-big-loops.pas"
 
 // Runtime options
