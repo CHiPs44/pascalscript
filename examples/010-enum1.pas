9c10f5c84ecea3c05ed7ae8e2a9c0c2fb1562a41
CHiPs44 <chips@chips44.fr>
2026-08-10 00:17:38 +0200

    enum & type def WIP

--- examples/010-enum1.pas
+++ examples/010-enum1.pas
@@ -3,12 +3,12 @@
         SPDX-FileCopyrightText: 2026 Christophe 'CHiPs' Petit <chips44@gmail.com>
         SPDX-License-Identifier: LGPL-3.0-or-later
 *)
-Program TypeDef0;
+Program Enum1;
 
 Var
     AnsiColor: (Black, Red, Green, Yellow, Blue, Magenta, Cyan, White);
 
 Begin
-    AnsiColor := Yellow;
-    WriteLn(AnsiColor);
+    For AnsiColor := Black To White Do
+        WriteLn('#', Ord(AnsiColor), ' ', AnsiColor);
 End.
